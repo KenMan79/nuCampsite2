fcd534ad1238eef7ba124ce152d614d157de2f26
Kenneth Luster <51566205+KenMan79@users.noreply.github.com>
2020-10-29 12:55:11 -0600

    First Commit

---
+++ css/notes/note script.js
@@ -0,0 +1,7 @@
+// From Bryan Hines to Everyone: (12:13)
+// npm install --save-dev node-sass@4.14.1
+
+// From Bryan Hines to Everyone: (12:23)
+// npm install --save-dev onchange@7.0.0 parallelshell@3.0.1
+
+// npm install--save - dev - E parallelshell @3 .0 .1
